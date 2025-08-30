9b4417cb4adaacd0068df0cb1d0274886f66c7b3
nugget-cloud <kunal.mcisma@gmail.com>
2025-08-31 01:57:02 +0530

    added day 2 logs

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 I cannot use any library except the python standard library just to make this fun , i will do this challenge again in go lang too just to extend my knowledege in go lang as i am very weak but here we are 
 
 ### Approach 1  
-This was the most naive approach with async sadly enough the code does not run with a billion rows altogether I get no output need to figure this out
+Day 1 :This was the most naive approach with async sadly enough the code does not run with a billion rows altogether I get no output need to figure this out
+
+Day 2 :Well the initial enthusiams has worn off and i have no clue how this all works turns out python does not support multithreading idek know what that means in depth but sure from what i know i need to use multiprocessing rather than asycnio , went ahead going to read code of people smarter than me to understand how this works as in my 1 month of experience reading code makes you better dev rather than writing code
--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 import asyncio
 import time
 import pprint
+from multiprocessing import Process
 
+FILE_PATH = "data/measurements.txt"
 city = {} 
 
 async def calculate(val:dict):
     for key , value in val.items():
         val[key]=[min(val[key]) , sum(val[key])/len(val[key]) , max(val[key])]
-    pprint.pprint(val) 
     return val
     
 async def row_wise():
-    file = await read_file("data/measurements.txt")
-    for row in file:
-        first_item, second_item = row.split(";")
-        if first_item in city:
-            city[first_item].append(float(second_item))
-        else:
-            city[first_item] = [float(second_item)]  
+    async for chunk in read_file():
+        for row in chunk:
+            first_item, second_item = row.split(";")
+            if first_item in city:
+                city[first_item].append(float(second_item))
+            else:
+                city[first_item] = [float(second_item)]  
     
-    pprint.pprint(city)
     return city
     
-async def read_file(file_path:str):
-    with open(file_path , 'r') as file:
-        content = file.readlines()
-        return content
+async def read_file():
+    with open(FILE_PATH , 'r') as file:
+        while True:
+            chunk = file.readlines(1_00_000)
+            if not chunk:
+                break
+            yield chunk
     
 async def main():
     start_time = time.perf_counter()
-    pprint.pprint("The calculation starts now")
-    result = await row_wise()
+    pprint.pprint(f"The calculation starts now {start_time}")
+    result = Process(target=row_wise)
     pprint.pprint("Row wise calculation done")
     result =  await calculate(result)
     end_time = time.perf_counter()
